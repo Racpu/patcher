85484bd90fb311e1737d01be30a9765851c0e9f9
Filipe Carreto <carreto.idn@gmail.com>
2018-08-18 12:10:42 +0100

    fix path and pixel set color patch

--- patcher.sh
+++ patcher.sh
@@ -8,12 +8,12 @@ git -C build						clean -dfqx
 git -C build						reset -q --hard
 git -C kernel/oneplus/msm8996		clean -dfqx
 git -C kernel/oneplus/msm8996		reset -q --hard
-git -C frameworks					clean -dfqx
-git -C frameworks					reset -q --hard
+git -C frameworks/base				clean -dfqx
+git -C frameworks/base				reset -q --hard
 
 
 ### Racpu's patches
 git -C build						apply $RACPU/build0.patch
 git -C kernel/oneplus/msm8996		apply $RACPU/remove-safetynet-flags0.patch
-git -C frameworks					apply $RACPU/enable-circular-icons0.patch
-git -C frameworks					apply $RACPU/set-pixel-accent-color0.patch
+git -C frameworks/base				apply $RACPU/enable-circular-icons0.patch
+git -C frameworks/base				apply $RACPU/set-pixel-accent-color0.patch
