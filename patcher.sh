6c9f0447e3083894b361322d976fe5faa301d882
Filipe Carreto <carreto.idn@gmail.com>
2018-08-18 04:07:13 +0100

    Fix path and enable accent pixel color and circular icons

--- patcher.sh
+++ patcher.sh
@@ -4,12 +4,16 @@
 RACPU=$PWD/patcher/racpu
 
 # Clean up first
-git -C build				clean -dfqx
-git -C build				reset -q --hard
-git -C safetynet			clean -dfqx
-git -C safetynet			reset -q --hard
+git -C build						clean -dfqx
+git -C build						reset -q --hard
+git -C kernel/oneplus/msm8996		clean -dfqx
+git -C kernel/oneplus/msm8996		reset -q --hard
+git -C frameworks					clean -dfqx
+git -C frameworks					reset -q --hard
 
 
 ### Racpu's patches
-git -C build				apply $RACPU/build0.patch
-git -C safetynet			apply $RACPU/remove-safetynet-flags0.patch
+git -C build						apply $RACPU/build0.patch
+git -C kernel/oneplus/msm8996		apply $RACPU/remove-safetynet-flags0.patch
+git -C frameworks					apply $RACPU/enable-circular-icons0.patch
+git -C frameworks					apply $RACPU/set-pixel-accent-color0.patch
